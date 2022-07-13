2c6a448dad4a9b2b9cc1200f20a6c45398740a2d
David Ruiz <david.ruizvargas@hotmail.com>
2022-07-12 20:51:26 -0500

    Se agregan las canciones al front consumiendo API

--- services/serviciospotify.js
+++ services/serviciospotify.js
@@ -5,6 +5,7 @@
 
 const URI ="https://api.spotify.com/v1/artists/1DdhScDGl9AceKnbvgkFgz/top-tracks?market=US"
 
+const URI2 ="https://api.spotify.com/v1/artists/5awFg11m6qCdmOsy8LxEvc/top-tracks?market=US"
 
 //2. A HACER QUE OME?
 //CONFIGURO LA PETICIÓN
@@ -12,7 +13,7 @@ const URI ="https://api.spotify.com/v1/artists/1DdhScDGl9AceKnbvgkFgz/top-tracks
 const PETICION={
     method:'GET',
     headers : {
-        Authorization:"Bearer BQAhwuTT7SL7fjIl4fG0q93nYdyArieqJout3vZif9Qg0yRj9a6gbxXGLdISEyMDddJpIFWUcmf2qa5c9Xd2yocM5NpjsB_BO34s1s9k3yo8gNu1s0F6YOO1OSWXH4DqaPY2wv0KyzYUeYXd3Wn9R_c3UNYiE1VTYWhF"
+        Authorization:"Bearer BQBbCSl5VDBFtdMbS_24QGmMjkGr0_ekb7W5n1XbD3afmQw_pmU8vU3WffgoIvx2c7vYAvcZto6SWCgeb06w3V0dDHRNprNZLkr0c8JwZW_-Jst5DVM7gJMpghwW8vmpFxSxQrUO9VdS_3NI6rDY86_y6pi41UWKK3Jc"
     }
 }
 
@@ -20,17 +21,87 @@ const PETICION={
 //CONSUMA EL API
 
 //promesa (funcion asincrona)
-fetch(URI,PETICION)
+fetch(URI2,PETICION)
 .then(function(respuesta){
         return respuesta.json()//me aseguro del formato de respuesta
 })
 .then((respuesta)=>{
     console.log(respuesta)
     console.log(respuesta.tracks)
-    respuesta.tracks.forEach((cancion)=>{
-        console.log(cancion.preview_url)
-    });
-})//hago lo que quiera con la respuesta
+   // respuesta.tracks.forEach((cancion)=>{
+     //   console.log(cancion.preview_url)
+    //});
+    pintarCanciones(respuesta.tracks);
+    })
+//hago lo que quiera con la respuesta
 .catch(function(respuestaError){
     console.log(respuestaError);
 })
+
+
+//FUNCION PARA PINTAR DATOS
+function pintarCanciones(canciones){
+    //pasos para pintar/hacer render a en la GUI
+
+    //1.CREO UNA REFERENCIA A LA BASE SOBRE LA CUAL VOY A PINTAR
+
+    let fila=document.getElementById('fila')
+
+
+    //2. Recorrer los datos que me llegan
+    canciones.forEach(function(cancion){
+        
+        //aplicando TRAVERSING ()
+        //2.1  Creando columnas
+        let columna = document.createElement("div")
+        columna.classList.add("col")
+
+        //2.2 Creando la tarjeta
+        let tarjeta = document.createElement("div")
+        tarjeta.classList.add("card","h-100","shadow")
+
+        //2.3 creando el campo de audio
+        let audio = document.createElement("audio");
+        
+     
+        audio.src=cancion.preview_url
+        audio.classList.add("w-100");
+        audio.setAttribute("controls", "controls")
+
+        //2.4 creando el nombre de la cancion
+        let nombreCancion=document.createElement("h5");
+        nombreCancion.classList.add("text-center");
+        nombreCancion.textContent=cancion.name
+
+    //2.5 creando la foto del album
+    let fotoAlbum=document.createElement("img")
+    fotoAlbum.classList.add("img-fluid","w-100");
+    fotoAlbum.src=cancion.album.images[0].url
+
+    //popularidad
+
+    let popularidad = document.createElement("h6");
+    popularidad.classList.add("text-center", "fw-bold","text-info")
+    popularidad.textContent="popularidad: "+cancion.popularity;
+
+    //track number
+    let number = document.createElement("h5");
+    number.classList.add("text-center", "fw-bold")
+    number.textContent=cancion.track_number
+
+        //FINAL DEFINO JERARQUIAS (PADRES E HIJOS)
+        tarjeta.appendChild(fotoAlbum)
+        tarjeta.appendChild(number)
+        tarjeta.appendChild(nombreCancion)
+        tarjeta.appendChild(popularidad);
+        tarjeta.appendChild(audio);
+       
+        
+       
+        columna.appendChild(tarjeta);
+        fila.appendChild(columna)
+
+    })
+
+
+}
